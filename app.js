432e067841428c1fd37c0d76593fd170648de995
Tuan Anh Le <anhtuanle2101@gmail.com>
2020-12-14 19:24:53 -0500

    added OOP

--- app.js
+++ app.js
@@ -1,134 +1,165 @@
-const map=new Map();
-let currPlayer=1;
-const newGameBtn=document.querySelector('#new-game');
-const mainBoard=document.querySelector('#board');
-let clickable=true;
-let trace=new Set();
 
-newGameBtn.addEventListener('click',function(e){
-    clickable=true;
-    //create empty map
-    boardInit();
-    //insert a board into the DOM
-    createBoard();
-    //if a column is pressed, execute the logic
-    updatePlayerDOM();
-    play();
-})
 
-const updatePlayerDOM=()=>{
-    console.log(currPlayer);
-    if (currPlayer===1){
-        document.querySelector('#info>div').classList.add('player1');
-        document.querySelector('#info>div').classList.remove('player2');
-    }else{
-        document.querySelector('#info>div').classList.add('player2');
-        document.querySelector('#info>div').classList.remove('player1');
+class Game{
+    constructor(height,width){
+        [this.height,this.width]=[height,width];
+        this.clickable=true;
+        this.map=new Map();
+        this.currPlayer=1;
+        this.player1=null;
+        this.player2=null;
     }
-}   
-
-const play=()=>{
-    document.querySelector('thead').addEventListener('click',function(e){
-        if (clickable&&e.target.tagName==='TH'){
-            const columnIdx=e.target.getAttribute('data-index');
-            if (!checkIsFilledAt(columnIdx)){
-                let idx=0;
-                while(idx<6){
-                    if (!!map.get(`${idx}${columnIdx}`))
-                        idx++;
-                    else{
-                        map.set(`${idx}${columnIdx}`,currPlayer);
-                        insertCircleDOM(`${idx}${columnIdx}`,currPlayer);
-                        checkWin(idx,columnIdx);
-                        break;
-                    } 
-                }
-                switchPlayer();
-            }
+    addPlayers(player1,player2){
+        if (player1 instanceof Player && player2 instanceof Player){
+            this.player1=player1;
+            this.player2=player2;
+            return true;
         }
-    });
-}
-
-const fourConnected=(...arr)=>{
-    let count=0;
-    for(let i=0;i<arr.length-1;i++){
-        if (typeof arr[i]==='number' && arr[i]===arr[i+1]){
-            count++;
-            if (count===3){
-                console.log(trace);
-                return true;
-            }
+        return false;
+    }
+    updatePlayerDOM(){
+        document.querySelector('#info>div').style.backgroundColor=this[`player${this.currPlayer}`].color
+        if (this.currPlayer===1){
+            document.querySelector('#info>div').classList.add('player1');
+            document.querySelector('#info>div').classList.remove('player2');
         }else{
-            count=0;
-            trace.clear();
+            document.querySelector('#info>div').classList.add('player2');
+            document.querySelector('#info>div').classList.remove('player1');
         }
+    }  
+    handleClick(){
+        document.querySelector('thead').addEventListener('click',function(e){
+            if (this.clickable&&e.target.tagName==='TH'){
+                const columnIdx=e.target.getAttribute('data-index');
+                if (!this.checkIsFilledAt(columnIdx)){
+                    let rowIdx=0;
+                    while(rowIdx<this.height){
+                        if (!!this.map.get(`${rowIdx}${columnIdx}`))
+                            rowIdx++;
+                        else{
+                            this.map.set(`${rowIdx}${columnIdx}`,this.currPlayer);
+                            this.insertCircleDOM(`${rowIdx}${columnIdx}`,this.currPlayer);
+                            this.checkWin(rowIdx,columnIdx);
+                            break;
+                        } 
+                    }
+                    this.switchPlayer();
+                }
+            }
+        }.bind(this));
     }
-    return false;
-}
-
-const checkWin=(r,c)=>{
-    r=parseInt(r);
-    c=parseInt(c);
-    if(
-        fourConnected(map.get(`${r}${c-3}`),map.get(`${r}${c-2}`),map.get(`${r}${c-1}`),map.get(`${r}${c}`),map.get(`${r}${c+1}`),map.get(`${r}${c+2}`),map.get(`${r}${c+3}`))||
-        fourConnected(map.get(`${r}${c}`),map.get(`${r-1}${c}`),map.get(`${r-2}${c}`),map.get(`${r-3}${c}`))||
-        fourConnected(map.get(`${r-3}${c-3}`),map.get(`${r-2}${c-2}`),map.get(`${r-1}${c-1}`),map.get(`${r}${c}`),map.get(`${r+1}${c+1}`),map.get(`${r+2}${c+2}`),map.get(`${r+3}${c+3}`))||
-        fourConnected(map.get(`${r+3}${c-3}`),map.get(`${r+2}${c-2}`),map.get(`${r+1}${c-1}`),map.get(`${r}${c}`),map.get(`${r-1}${c+1}`),map.get(`${r-2}${c+2}`),map.get(`${r-3}${c+3}`))
-    ){
-        clickable=false;
-        setTimeout(function(){
-            alert(currPlayer+'won');
-        },500);
-    }
-    
-}
-
-const switchPlayer=()=>{
-    currPlayer=currPlayer===1?2:1;
-    // console.log(currPlayer);
-    updatePlayerDOM();
-}
-
-const boardInit=()=>{
-    mainBoard.innerText='';
-    for (let i=0;i<7;i++)
-        for (let j=0;j<6;j++)
-            map.set(`${i}${j}`,null);
-}
-
-
-const insertCircleDOM=(location,player)=>{
-    const circle=document.createElement('div');
-    circle.classList.add('player'+player);
-    console.log(location);
-    const container=document.querySelector(`td[data-location="${location}"`)
-    container.append(circle);
-}
-
-const checkIsFilledAt=column=>{
-    return !!map.get(`5${column}`);
-}
-
-const createBoard=()=>{
-    const board=document.createElement('table');
-    const thead=document.createElement('thead');
-    for(let i=0;i<7;i++){
-        const th=document.createElement('th');
-        th.setAttribute('data-index',i);
-        thead.append(th);
-    }
-    board.append(thead);
-    const tbody=document.createElement('tbody');
-    for (let i=5;i>=0;i--){
-        const tr=document.createElement('tr');
-        for (let j=0;j<7;j++){
-            const td=document.createElement('td');
-            td.setAttribute('data-location',`${i}${j}`);
-            tr.append(td);
+    fourConnected(...arr){
+        let count=0;
+        for(let i=0;i<arr.length-1;i++){
+            if (typeof arr[i]==='number' && arr[i]===arr[i+1]){
+                count++;
+                if (count===3){
+                    return true;
+                }
+            }else{
+                count=0;
+            }
         }
-        tbody.append(tr);
+        return false;
     }
-    board.append(tbody);
-    mainBoard.append(board);
+    checkWin(r,c){
+        r=parseInt(r);
+        c=parseInt(c);
+        if(
+            this.fourConnected(this.map.get(`${r}${c-3}`),this.map.get(`${r}${c-2}`),this.map.get(`${r}${c-1}`),this.map.get(`${r}${c}`),this.map.get(`${r}${c+1}`),this.map.get(`${r}${c+2}`),this.map.get(`${r}${c+3}`))||
+            this.fourConnected(this.map.get(`${r}${c}`),this.map.get(`${r-1}${c}`),this.map.get(`${r-2}${c}`),this.map.get(`${r-3}${c}`))||
+            this.fourConnected(this.map.get(`${r-3}${c-3}`),this.map.get(`${r-2}${c-2}`),this.map.get(`${r-1}${c-1}`),this.map.get(`${r}${c}`),this.map.get(`${r+1}${c+1}`),this.map.get(`${r+2}${c+2}`),this.map.get(`${r+3}${c+3}`))||
+            this.fourConnected(this.map.get(`${r+3}${c-3}`),this.map.get(`${r+2}${c-2}`),this.map.get(`${r+1}${c-1}`),this.map.get(`${r}${c}`),this.map.get(`${r-1}${c+1}`),this.map.get(`${r-2}${c+2}`),this.map.get(`${r-3}${c+3}`))
+        ){
+            this.clickable=false;
+            setTimeout(function(){
+                alert('Player '+this.currPlayer+' have won');
+            }.bind(this),500);
+        }
+        
+    }
+    switchPlayer(){
+        this.currPlayer=this.currPlayer===1?2:1;
+        this.updatePlayerDOM();
+    }
+    boardInit(height,width){
+        document.querySelector('#board').innerText='';
+        for (let i=0;i<width;i++)
+            for (let j=0;j<height;j++)
+                this.map.set(`${i}${j}`,null);
+    }
+    insertCircleDOM(location,player){
+        const circle=document.createElement('div');
+        circle.classList.add('player'+player);
+        circle.style.backgroundColor=this[`player${this.currPlayer}`].color;
+        const container=document.querySelector(`td[data-location="${location}"`)
+        container.append(circle);
+    }
+    checkIsFilledAt(column){
+        return !!this.map.get(`${this.height-1}${column}`);
+    } 
+    createHTMLBoard(){
+        const board=document.createElement('table');
+        const thead=document.createElement('thead');
+        for(let i=0;i<this.width;i++){
+            const th=document.createElement('th');
+            th.setAttribute('data-index',i);
+            thead.append(th);
+        }
+        board.append(thead);
+        const tbody=document.createElement('tbody');
+        for (let i=this.height-1;i>=0;i--){
+            const tr=document.createElement('tr');
+            for (let j=0;j<7;j++){
+                const td=document.createElement('td');
+                td.setAttribute('data-location',`${i}${j}`);
+                tr.append(td);
+            }
+            tbody.append(tr);
+        }
+        board.append(tbody);
+        document.querySelector('#board').append(board);
+    }
+    play(){
+        //empty map created
+        this.boardInit();
+        //implement the board (map) into DOM
+        this.createHTMLBoard();
+        //show the current player color
+        this.updatePlayerDOM();
+        //handle clicks
+        this.handleClick();
+    }
 }
+
+class Player{
+    constructor(color){
+        this.color=color;
+    }
+}
+
+// const newGameBtn=document.querySelector('#new-game');
+// newGameBtn.addEventListener('click',function(e){
+//     e.preventDefault();
+//     clickable=true;
+//     const gameInstance=new Game(6,7);
+//     const player1=new Player(document.querySelector('#player1').value);
+//     const player2=new Player(document.querySelector('#player2').value);
+//     console.log(player1.color,player2.color);
+//     if (player1.color && player2.color)
+//         gameInstance.play();
+// })
+document.querySelector('form').addEventListener('click',function(e){
+    e.preventDefault();
+    if (e.target.tagName==='BUTTON'){
+        clickable=true;
+        const gameInstance=new Game(6,7);
+        const player1=new Player(document.querySelector('#player1').value);
+        const player2=new Player(document.querySelector('#player2').value);
+       
+        if (!!(player1.color && player2.color))
+            gameInstance.addPlayers(player1,player2);
+            gameInstance.play();
+        }
+})
 
