a14174779c8c6d30ec20626944efd015352d95b9
Tuan Anh Le <tuananhle@Tuans-iMac.local>
2020-12-21 12:01:57 -0500

    added comments

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ class Game{
     constructor(height,width){
         [this.height,this.width]=[height,width];
         this.clickable=true;
-        this.map=new Map();
+        this.board=new Map();
         this.currPlayer=1;
         this.player1=null;
         this.player2=null;
     }
+    //Add player objects into the game object
     addPlayers(player1,player2){
         if (player1 instanceof Player && player2 instanceof Player){
             this.player1=player1;
@@ -17,6 +18,7 @@ class Game{
         }
         return false;
     }
+    //When the player is switched, the corresponding player's color is switched too
     updatePlayerDOM(){
         document.querySelector('#info>div').style.backgroundColor=this[`player${this.currPlayer}`].color
         if (this.currPlayer===1){
@@ -27,6 +29,7 @@ class Game{
             document.querySelector('#info>div').classList.remove('player1');
         }
     }  
+    //add event handler to the first row 
     handleClick(){
         document.querySelector('thead').addEventListener('click',function(e){
             if (this.clickable&&e.target.tagName==='TH'){
@@ -34,10 +37,10 @@ class Game{
                 if (!this.checkIsFilledAt(columnIdx)){
                     let rowIdx=0;
                     while(rowIdx<this.height){
-                        if (!!this.map.get(`${rowIdx}${columnIdx}`))
+                        if (!!this.board.get(`${rowIdx}${columnIdx}`))
                             rowIdx++;
                         else{
-                            this.map.set(`${rowIdx}${columnIdx}`,this.currPlayer);
+                            this.board.set(`${rowIdx}${columnIdx}`,this.currPlayer);
                             this.insertCircleDOM(`${rowIdx}${columnIdx}`,this.currPlayer);
                             this.checkWin(rowIdx,columnIdx);
                             break;
@@ -48,6 +51,7 @@ class Game{
             }
         }.bind(this));
     }
+    //check if there are four same color connected  
     fourConnected(...arr){
         let count=0;
         for(let i=0;i<arr.length-1;i++){
@@ -62,15 +66,15 @@ class Game{
         }
         return false;
     }
+    //check win conditions at current position with row and column
     checkWin(r,c){
         r=parseInt(r);
         c=parseInt(c);
-        if(
-            this.fourConnected(this.map.get(`${r}${c-3}`),this.map.get(`${r}${c-2}`),this.map.get(`${r}${c-1}`),this.map.get(`${r}${c}`),this.map.get(`${r}${c+1}`),this.map.get(`${r}${c+2}`),this.map.get(`${r}${c+3}`))||
-            this.fourConnected(this.map.get(`${r}${c}`),this.map.get(`${r-1}${c}`),this.map.get(`${r-2}${c}`),this.map.get(`${r-3}${c}`))||
-            this.fourConnected(this.map.get(`${r-3}${c-3}`),this.map.get(`${r-2}${c-2}`),this.map.get(`${r-1}${c-1}`),this.map.get(`${r}${c}`),this.map.get(`${r+1}${c+1}`),this.map.get(`${r+2}${c+2}`),this.map.get(`${r+3}${c+3}`))||
-            this.fourConnected(this.map.get(`${r+3}${c-3}`),this.map.get(`${r+2}${c-2}`),this.map.get(`${r+1}${c-1}`),this.map.get(`${r}${c}`),this.map.get(`${r-1}${c+1}`),this.map.get(`${r-2}${c+2}`),this.map.get(`${r-3}${c+3}`))
-        ){
+        rowCondition=this.fourConnected(this.board.get(`${r}${c-3}`),this.board.get(`${r}${c-2}`),this.board.get(`${r}${c-1}`),this.board.get(`${r}${c}`),this.board.get(`${r}${c+1}`),this.board.get(`${r}${c+2}`),this.board.get(`${r}${c+3}`));
+        columnCondition=this.fourConnected(this.board.get(`${r}${c}`),this.board.get(`${r-1}${c}`),this.board.get(`${r-2}${c}`),this.board.get(`${r-3}${c}`));
+        dia1Condition=this.fourConnected(this.board.get(`${r-3}${c-3}`),this.board.get(`${r-2}${c-2}`),this.board.get(`${r-1}${c-1}`),this.board.get(`${r}${c}`),this.board.get(`${r+1}${c+1}`),this.board.get(`${r+2}${c+2}`),this.board.get(`${r+3}${c+3}`));
+        dia2Condition=this.fourConnected(this.board.get(`${r+3}${c-3}`),this.board.get(`${r+2}${c-2}`),this.board.get(`${r+1}${c-1}`),this.board.get(`${r}${c}`),this.board.get(`${r-1}${c+1}`),this.board.get(`${r-2}${c+2}`),this.board.get(`${r-3}${c+3}`));
+        if(rowCondition||columnCondition||dia1Condition||dia2Condition){
             this.clickable=false;
             setTimeout(function(){
                 alert('Player '+this.currPlayer+' have won');
@@ -78,16 +82,19 @@ class Game{
         }
         
     }
+    //switch player after a turn
     switchPlayer(){
         this.currPlayer=this.currPlayer===1?2:1;
         this.updatePlayerDOM();
     }
+    //create a empty board
     boardInit(height,width){
         document.querySelector('#board').innerText='';
         for (let i=0;i<width;i++)
             for (let j=0;j<height;j++)
-                this.map.set(`${i}${j}`,null);
+                this.board.set(`${i}${j}`,null);
     }
+    //insert circle into the board (DOM)
     insertCircleDOM(location,player){
         const circle=document.createElement('div');
         circle.classList.add('player'+player);
@@ -95,9 +102,11 @@ class Game{
         const container=document.querySelector(`td[data-location="${location}"`)
         container.append(circle);
     }
+    //check if the current column is full
     checkIsFilledAt(column){
-        return !!this.map.get(`${this.height-1}${column}`);
+        return !!this.board.get(`${this.height-1}${column}`);
     } 
+    //create HTML board in DOM
     createHTMLBoard(){
         const board=document.createElement('table');
         const thead=document.createElement('thead');
@@ -120,8 +129,9 @@ class Game{
         board.append(tbody);
         document.querySelector('#board').append(board);
     }
+    //Play steps
     play(){
-        //empty map created
+        //empty board created
         this.boardInit();
         //implement the board (map) into DOM
         this.createHTMLBoard();
@@ -138,17 +148,7 @@ class Player{
     }
 }
 
-// const newGameBtn=document.querySelector('#new-game');
-// newGameBtn.addEventListener('click',function(e){
-//     e.preventDefault();
-//     clickable=true;
-//     const gameInstance=new Game(6,7);
-//     const player1=new Player(document.querySelector('#player1').value);
-//     const player2=new Player(document.querySelector('#player2').value);
-//     console.log(player1.color,player2.color);
-//     if (player1.color && player2.color)
-//         gameInstance.play();
-// })
+//Form submit handler
 document.querySelector('form').addEventListener('click',function(e){
     e.preventDefault();
     if (e.target.tagName==='BUTTON'){
@@ -156,10 +156,10 @@ document.querySelector('form').addEventListener('click',function(e){
         const gameInstance=new Game(6,7);
         const player1=new Player(document.querySelector('#player1').value);
         const player2=new Player(document.querySelector('#player2').value);
-       
+        
         if (!!(player1.color && player2.color))
             gameInstance.addPlayers(player1,player2);
             gameInstance.play();
-        }
+    }
 })
 
